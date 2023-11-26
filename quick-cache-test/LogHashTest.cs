8c76b92677cd4cb57e49b37692c61649f9adb470
reubenh <reubenh@teamhitori.com>
2023-11-26 19:46:51 +0000

    Add LogHash + Unit Tests

---
+++ quick-cache-test/LogHashTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamHitori.QuickCache;
+
+namespace TeamHitori.QuickCacheTest
+{
+    [TestClass]
+    public class LogHashTest
+    {
+        [TestMethod]
+        public void GetLogPosition_ShouldRetrieveCorrectPositionForExistingKey()
+        {
+            var logHash = new LogHash();
+            string key = "testKey";
+            ulong expectedPosition = 123; // Example position
+            logHash.Set(key, expectedPosition);
+
+            var actualPosition = logHash.GetLogPosition(key);
+
+            Assert.AreEqual(expectedPosition, actualPosition);
+        }
+
+        [TestMethod]
+        public void GetLogPosition_ShouldReturnNullForNonExistentKey()
+        {
+            var logHash = new LogHash();
+            string nonExistentKey = "nonExistentKey";
+
+            var position = logHash.GetLogPosition(nonExistentKey);
+
+            Assert.IsNull(position);
+        }
+
+        [TestMethod]
+        public void GetLogPosition_ShouldReturnNullAfterKeyIsRemoved()
+        {
+            var logHash = new LogHash();
+            string key = "testKey";
+            logHash.Set(key, 123);
+            logHash.RemoveKey(key);
+
+            var position = logHash.GetLogPosition(key);
+
+            Assert.IsNull(position);
+        }
+
+        [TestMethod]
+        public void GetLogPosition_ShouldBeThreadSafeForConcurrentAccess()
+        {
+            var logHash = new LogHash();
+            string key = "testKey";
+            ulong expectedPosition = 123;
+            logHash.Set(key, expectedPosition);
+            var tasks = new List<Task<ulong?>>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                tasks.Add(Task.Run(() => logHash.GetLogPosition(key)));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            foreach (var task in tasks)
+            {
+                Assert.AreEqual(expectedPosition, task.Result);
+            }
+        }
+
+        [TestMethod]
+        public void GetLogPosition_ShouldDistinguishSimilarKeys()
+        {
+            var logHash = new LogHash();
+            string key1 = "key";
+            string key2 = "key2";
+            ulong position1 = 123;
+            ulong position2 = 456;
+            logHash.Set(key1, position1);
+            logHash.Set(key2, position2);
+
+            var retrievedPosition1 = logHash.GetLogPosition(key1);
+            var retrievedPosition2 = logHash.GetLogPosition(key2);
+
+            Assert.AreEqual(position1, retrievedPosition1);
+            Assert.AreEqual(position2, retrievedPosition2);
+        }
+
+
+
+    }
+}
---
+++ quick-cache/LogHash.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamHitori.QuickCache
+{
+    internal class LogHash
+    {
+        ConcurrentDictionary<string, ulong> _hash = new ConcurrentDictionary<string, ulong>();
+
+        public ulong? GetLogPosition(string key)
+        {
+            var hasValue = this._hash.TryGetValue(key, out ulong value);
+            return hasValue ? value : null;
+        }
+        public void Set(string key, ulong logPosition)
+        {
+            this._hash.TryAdd(key, logPosition);
+        }
+
+        public void RemoveKey(string key)
+        {
+            this._hash.TryRemove(key, out ulong value);
+        }
+        public ulong[] GetAllReferences()
+        {
+            return this._hash.Values.ToArray();
+        }
+    }
+}
